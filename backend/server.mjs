e4434f915fa1048b454d88558b8cac78887f704f
Abderrahim <dicou940@gmail.com>
2025-09-21 15:47:05 +0100

    fix origin

--- backend/server.mjs
+++ backend/server.mjs
@@ -14,8 +14,7 @@ cloudinaryConnect()
 
 app.use(express.json())
 app.use(cors({
-  origin: ["https://prescripto-j6cq.vercel.app", "https://prescripto-7k1g.vercel.app"],
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  origin: ["http://localhost:5173"],
   credentials: true
 }))
 app.use(express.urlencoded({ extended: true }))
