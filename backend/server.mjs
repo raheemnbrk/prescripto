31987541d08e7fb9e2501fb30a31fb4d317f6769
Abderrahim <dicou940@gmail.com>
2025-09-25 11:52:48 +0100

    fix cors

--- backend/server.mjs
+++ backend/server.mjs
@@ -9,13 +9,28 @@ import userRouter from './routes/userRoute.mjs'
 import dotenv from "dotenv"
 dotenv.config()
 
+const allowedOrigins = [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "https://prescripto-five-lac.vercel.app"
+]
+
 const app = express()
 const port = process.env.PORT || 3000
 connectDb()
 cloudinaryConnect()
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true)
+        } else {
+            callback(new Error("Not allowed by CORS"))
+        }
+    },
+    credentials: true
+}))
 app.use(express.urlencoded({ extended: true }))
 
 app.use('/api/admin', adminRouter)
